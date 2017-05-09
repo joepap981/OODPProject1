919b2ed7fb3dca627c64a03eba8ad62eff752080
joepap981 <joepap981@gmail.com>
2017-05-10 03:48:29 +0900

    Delete ProjectInterface.java

--- ProjectInterface.java
+++
@@ -1,9 +0,0 @@
-import java.io.IOException;
-
-/**
- * Created by Yehwan on 5/9/2017.
- */
-public interface ProjectInterface{
-    public Coordinates[] getCoor(String fileName) throws IOException;
-    public int numOfCoordinates (String fileName) throws IOException;
-}
